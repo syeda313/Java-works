9bf3e991aa641fba73f30b9e85b7f2d45539e44e
Reem Naqvi <reemnaqvi@gmail.com>
2018-05-31 17:12:12 -0500

    Create CountTokens.java

---
+++ CountTokens.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+
+import stdlib.StdIn;
+import stdlib.StdOut;
+
+public class CountTokens {
+
+	public static void main(String[] args) {
+		StdOut.print("Please enter the text file pathname: ");
+		String filename = StdIn.readLine();
+		StdIn.fromFile(filename);
+		final String bookText = StdIn.readAll();
+		String[] token = bookText.split("\n");
+
+		ArrayList<String> xml = new ArrayList<String>();
+
+		for (String x : token) {
+			String[] a = x.split(" ");
+			for (String word : a) {
+				xml.add(word);
+			}
+		}
+		int count = 0;
+		int open = 0;
+		int close = 0;
+		for (String w : xml) {
+			if (!w.equals("")) {
+				XMLToken t = new XMLToken(w);
+
+				if (!t.isTag()) {
+					count++;
+				}
+				if (t.isOpeningTag()) {
+					open++;
+				} else if (t.isClosingTag()) { // counts bad closed tags and
+					close++;
+				}
+			}
+		}
+		StdOut.println("The number of words in the XML file is " + count);
+		StdOut.println("The number of Opening tag in the XML file is " + open);
+		StdOut.println("The number of Closing tag in the XML file is " + close);
+
+	}
+}
